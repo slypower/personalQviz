ec94a9070d9edcfc6592503dfb0709abd06cc0de
Vital <slyvitax@mail.ru>
2022-03-10 20:58:22 +0300

    Initial Commit

---
+++ personalQviz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  personalQviz
+//
+//  Created by Виталий Бобрик on 10.03.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
