7701e7f5b1867412320002d8863d2d98ed48b418
Vital <slyvitax@mail.ru>
2022-03-15 21:25:58 +0300

    delete button Back from result view

--- PersonalQuiz/PersonalQuiz/ViewControllers/ResultViewController.swift
+++ PersonalQuiz/PersonalQuiz/ViewControllers/ResultViewController.swift
@@ -9,8 +9,15 @@ import UIKit
 
 class ResultViewController: UIViewController {
 
+    //1. Передать массив с ответами на экран с результатами
+    //2. Определить наиболее часто встречающийся тип животного
+    //3. Отобразить результаты в соответствии с этим животным
+    //4. Избавиться от кновки возврата назаж на экране результатов +
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        navigationItem.hidesBackButton = true
 
     }
+    
 }
